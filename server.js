0af72054378b4db08a7c8a8697ddca39b1360dc8
enandan <jl830630@gmail.com>
2020-05-28 15:24:33 -0700

    update port to support heroko

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 
